8c19052f9053f0ba59e89fe8de5cd983f33288b0
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2022-12-29 20:44:25 +0200

    dfd

--- js/test.js
+++ js/test.js
@@ -1,2 +1,9 @@
-const array = 13 > "13";
-console.log(array);
+const doMath = function (a, b, c, callback) {
+  return callback(a, b, c);
+};
+
+const plus = function (x, y, z) {
+  return x + y + z;
+};
+
+console.log(doMath(3, 5, 4, plus));
